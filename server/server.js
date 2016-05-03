7c9cb626378ff113320b99326faa39abde01a232
jpoehler <jpoehler@comcast.net>
2016-05-03 11:24:21 -0500

    some updates

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname, '/public/', file));
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
   console.log('Listening on port: ', app.get('port'));
