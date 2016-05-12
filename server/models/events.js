94ea0858ff4d787165ff0e9deee019b16a9689ce
jpoehler <jpoehler@comcast.net>
2016-05-12 15:59:20 -0500

    connections

--- server/models/events.js
+++ server/models/events.js
@@ -6,8 +6,9 @@ var pg = require('pg');
 
 var connectionString = '';
 
-if(process.env.DATABASE_URL != undefined){
-  connectionString = process.env.DATABASE_URL + 'ssl';
+if(process.env.DATABASE_URL){
+  pg.defaults.ssl=true;
+  connectionString = process.env.DATABASE_URL;
 } else {
   connectionString = 'postgres://localhost:5432/passport-users';
 }
--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,9 @@ var router = express.Router();
 var pg = require('pg');
 
 var connectionString = '';
-if(process.env.DATABASE_URL != undefined){
-  connectionString = process.env.DATABASE_URL + 'ssl';
+if(process.env.DATABASE_URL){
+  pg.defaults.ssl=true;
+  connectionString = process.env.DATABASE_URL;
 } else {
   connectionString = 'postgres://localhost:5432/passport-users';
 }
