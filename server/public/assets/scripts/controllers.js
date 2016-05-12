ece5dac9ddcb1aa4bff8df9c2e52125b687ee82f
jpoehler <jpoehler@comcast.net>
2016-05-12 11:05:49 -0500

    fixed register button

--- server/public/assets/scripts/controllers.js
+++ server/public/assets/scripts/controllers.js
@@ -1,4 +1,4 @@
-myApp.controller('UserController', ['$scope', '$http', 'UserService', function($scope, $http, UserService) {
+myApp.controller('UserController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, UserService) {
   var userObject = {};
   var userService = UserService;
 
@@ -11,7 +11,7 @@ myApp.controller('UserController', ['$scope', '$http', 'UserService', function($
   console.log("getting user info from the user controller");
 
   $scope.redirect = function(){
-    window.location = '#/register.html';
+    $location.path = '#/register.html';
   };
 
 }]);
