f80d9619eda55458f027902a56d7c09c4c3616cc
jpoehler <jpoehler@comcast.net>
2016-05-12 13:44:39 -0500

    user.js update

--- server/models/user.js
+++ server/models/user.js
@@ -36,10 +36,10 @@ router.get('/', function(req, res) {
     // check if logged in
     if(req.isAuthenticated()) {
         // send back user object from database
-        res.status(status).send(req.user);
+        res.send(req.user);
     } else {
         // failure best handled on the server. do redirect here.
-        res.status(status).send(false);
+        res.send(false);
     }
 });
 
